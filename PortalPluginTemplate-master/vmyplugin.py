07d7f52d02f786e234c6011da9c0f44871456f60
MasterMG2020 <markus.gaigg2020@gmail.com>
2021-09-16 10:56:45 +0200

    added message

--- PortalPluginTemplate-master/vmyplugin.py
+++ PortalPluginTemplate-master/vmyplugin.py
@@ -21,6 +21,7 @@ class HelloWorldView(ClassView):
                 
         extra_context = {'plugin_message':_("Hello World")}
         messages.success(self.request, message=_("Hello World message successfully sent"))
+        messages.success(self.request, message=_("SUS"))
         log.debug("Hello World message successfully sent")
         return self.main(self.request, self.template, extra_context)
 
