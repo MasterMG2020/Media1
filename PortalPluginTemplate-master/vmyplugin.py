673c0ab66950accff4137b5a0927487f7efb6ebd
MasterMG2020 <markus.gaigg2020@gmail.com>
2021-09-16 15:18:37 +0200

    fixed django.usrls bug

--- PortalPluginTemplate-master/vmyplugin.py
+++ PortalPluginTemplate-master/vmyplugin.py
@@ -8,7 +8,7 @@ from .models import MyPluginModel
 from django.shortcuts import get_object_or_404
 from .forms import MyPluginForm
 from django.http import HttpResponseRedirect
-from django.usrls import reverse
+from django.urls import reverse
 from portal.vidispine.iuser import UserHelper
 from portal.vidispine.igeneral import performVSAPICall
 log = logging.getLogger(__name__)
