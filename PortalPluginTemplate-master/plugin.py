872a89e5068cda9ecc7fb3788bb4d74449cdcc9f
MasterMG2020 <markus.gaigg2020@gmail.com>
2021-09-16 17:33:26 +0200

    added abit of SUS

--- PortalPluginTemplate-master/plugin.py
+++ PortalPluginTemplate-master/plugin.py
@@ -105,57 +105,33 @@ class MyGearboxMenuPlugin(Plugin):
         return {'guid': self.plugin_guid, 'template': 'portalplugintemplate/gearbox_menu.html'}
 
 
-MyGearboxMenuPlugin()
+# MyGearboxMenuPlugin()
 
 
-class ItemContextPlugin(Plugin):
-    implements(IContextProcessor) 
+# class ItemContextPlugin(Plugin):
+#     implements(IContextProcessor) 
     
-    def __init__(self):
-        self.name = "ItemContextPlugin"
-        # Define a GUID for each plugin.
-        # Use e.g. http://www.guidgenerator.com/
-        self.plugin_guid = "7d024a0e-47ea-45b7-93b9-ae115a8ee7fa"
-        log.debug("Initiated ItemContextPlugin")
+#     def __init__(self):
+#         self.name = "ItemContextPlugin"
+#         # Define a GUID for each plugin.
+#         # Use e.g. http://www.guidgenerator.com/
+#         self.plugin_guid = "7d024a0e-47ea-45b7-93b9-ae115a8ee7fa"
+#         log.debug("Initiated ItemContextPlugin")
         
-    def __call__(self,context, class_object):
-        from portal.vidispine.vitem import ItemView
-        if isinstance(class_object, ItemView) is False:
-            return context
+#     def __call__(self,context, class_object):
+#         from portal.vidispine.vitem import ItemView
+#         if isinstance(class_object, ItemView) is False:
+#             return context
 
-        self.context = context
-        self.class_object = class_object 
-        #self.username = "test" 
-        return self.process_context()
+#         self.context = context
+#         self.class_object = class_object 
+#         #self.username = "test" 
+#         return self.process_context()
     
-    def process_context(self):
-        extra_context = self.context.dicts[len(self.context.dicts)-1]
-        # Do plugin stuff here and modify the extra_context
-        # extra_context['my_new_data_key'] = 'my new data value' 
+#     def process_context(self):
+#         extra_context = self.context.dicts[len(self.context.dicts)-1]
+#         # Do plugin stuff here and modify the extra_context
+#         # extra_context['my_new_data_key'] = 'my new data value' 
 
-        return self.context
+#         return self.context
 
-
-ItemContextPlugin()
-
-
-class PortalPluginTemplatePluginRegister(Plugin):
-    implements(IAppRegister)
-
-    def __init__(self):
-        self.name = "PortalPluginTemplateRegister"
-        self.plugin_guid = "2b622e08-e442-44da-ac5b-cd73f3fc7e70"
-        log.debug('Registered the PortalPluginTemplate App')
-
-    def __call__(self):
-        from .__init__ import __version__ as versionnumber
-        _app_dict = {
-            'name': 'PortalPluginTemplate Plugin',
-            'version': versionnumber,
-            'author': 'Cantemo AB',
-            'author_url': 'www.cantemo.com',
-            'notes': 'Copyright 2012-2015. All Rights Reserved'}
-        return _app_dict
-
-
-PortalPluginTemplatePluginRegister()
